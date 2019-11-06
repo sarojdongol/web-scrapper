68414d5acdc163419991c8d9f36d922292d82dab
Saroj Dongol <sarojdongol@gmail.com>
2019-11-07 00:39:57 +1100

    Added class specific

--- GetDataUrl.py
+++ GetDataUrl.py
@@ -7,8 +7,6 @@ import ssl
 # This restores the same behavior as before.
 context = ssl._create_unverified_context()
 
-parser = argparse.ArgumentParser(description='Web Scrapper Arguments!')
-parser.add_argument("--U", help="Target website URL")
 
 def WebScrapyer(url):
     try:
@@ -17,9 +15,16 @@ def WebScrapyer(url):
             return "URL not found"
         else:
             bsObj = BeautifulSoup(html,'html.parser')
-            content_lis = bsObj.find_all('div', {'class': 'sydney-cruise-left tab-group'})
-            return content_lis
-            
+            #dinner-items sydney-cruise-box-left-img   sydney-cruise-left tab-group
+            #soup = bsObj.find_all('div', {'class': 'sydney-cruise-right'})
+            #for links in soup:
+            #    return links.find('a')['href'] 
+
+            for item in bsObj.find_all(class_=["sydney-cruise-right"]):
+                    product_id = item.find(class_='child-navs tab-cnt hide dinner-tab')
+                    description = item.find(class_='cruise-text-box')
+                    print(description)
+
 
     except (AttributeError,HTTPError) as err:
         print("Please provide correct  website url",err)
